5d489599cc30309c50fd66eb80f593ee344bd47a
isaiah973 <adeolaisaiah01@gmail.com>
2026-05-04 23:36:10 +0100

    commit

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import API from "../api/axios";
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await API.get("/products/get-products");
+
+        setProducts(res.data.allProducts || []);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+
+      {products.length === 0 ? (
+        <p>No products found</p>
+      ) : (
+        products.map((p) => (
+          <div key={p._id}>
+            <h3>{p.title}</h3>
+            <img src={p.image} width="150" />
+            <p>₦{p.price}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Home;
